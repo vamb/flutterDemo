f11ede7062b95c0ddf71c6234a80d539c0ede8c7
zhangsy <zhangsy@think-land.com>
2024-01-15 17:40:15 +0800

    feat: P12 commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,76 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      // theme: ThemeData.dark(),
-      home: Home())
+      MaterialApp(
+        // theme: ThemeData.dark(),
+          home: Home())
   );
 }
 
-class Home extends StatelessWidget {
+
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+
+}
+
+class _HomeState extends State<Home> {
+
+  var count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-      AppBar(
-        title: Text("test",),
-        centerTitle: true,
-        // backgroundColor: Colors.redAccent,
-        backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+        title: Text('这是一个标题',),
+        backgroundColor: Color.fromRGBO(255, 50, 50, 0.8),
       ),
-      // body: Text('test message'),
-      body:
-        // Image.asset(
-        //   '../assets/1.jpg', height: 120, width: 120,
-        //   // fit: BoxFit.fill,
-        //   fit: BoxFit.cover,
-        //   // repeat: ImageRepeat.repeat,
-        // ),
-
-        // Image.network('https://ts1.cn.mm.bing.net/th/id/R-C.1f5914fce91db58cd69dd3079cf577fd?rik=h0ejjMK7AuWeZg&riu=http%3a%2f%2fwww.brooklyncomicshop.com%2fBCS%2fwp-content%2fuploads%2f2019%2f02%2fAction-Comics-419-Brooklyn-Comic-Shop.jpg.jpeg&ehk=sL0vX4cvv1WKTcd1MO6toEy4Oi8PpKrvK5G4Hy4xEdc%3d&risl=&pid=ImgRaw&r=0',
-        // width: 120, height: 120, fit: BoxFit.fill,)
-
-        // CircleAvatar(backgroundImage: AssetImage('../assets/2.webp'), radius: 50,)
-
-        // FadeInImage(placeholder: AssetImage('../assets/3.webp'), image: NetworkImage('https://ts1.cn.mm.bing.net/th/id/R-C.1f5914fce91db58cd69dd3079cf577fd?rik=h0ejjMK7AuWeZg&riu=http%3a%2f%2fwww.brooklyncomicshop.com%2fBCS%2fwp-content%2fuploads%2f2019%2f02%2fAction-Comics-419-Brooklyn-Comic-Shop.jpg.jpeg&ehk=sL0vX4cvv1WKTcd1MO6toEy4Oi8PpKrvK5G4Hy4xEdc%3d&risl=&pid=ImgRaw&r=0'),)
-        Row(
-          children: [
-            Text('text1'),
-            Text('text2'),
-            Text('text3'),
-            Text('text4'),
-            Text('text5'),
-            Column(
-              children: [
-                Text('text6'),
-                Text('text7'),
-                Image.asset(
-                  '../assets/1.jpg', height: 120, width: 120,
-                  // fit: BoxFit.fill,
-                  fit: BoxFit.cover,
-                  // repeat: ImageRepeat.repeat,
-                ),
-                ElevatedButton(
-                  onPressed: (){print('ElevatedButton btn click');},
-                  child: Text('ElevatedButton')),
-                TextButton(
-                  onPressed: (){print('TextButton btn click');},
-                  child: Text('TextButton')),
-                OutlinedButton(
-                  onPressed: (){print('OutlinedButton btn click');},
-                  child: Text('OutlinedButton')),
-              ],
-            ),
-            Text('text8'),
-            Text('text9'),
-          ],
-        ),
+      body: Container(
+        color: Colors.greenAccent,
+        child: Text(count.toString()),
+        width: 200, height: 200,
+        // alignment: Alignment(-1, -1),
+        padding: EdgeInsets.symmetric(vertical: 90, horizontal: 90),
+      ),
       floatingActionButton: FloatingActionButton(
-          onPressed: (){print('FloatingActionButton btn click');},
-          child: Icon(Icons.add),
-      )
+        child: Icon(Icons.add),
+        onPressed: (){
+          setState(() {
+            count++;
+          });
+        },
+      ),
     );
   }
 
 }
-
---
+++ lib/main_v1.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      // theme: ThemeData.dark(),
+      home: Home())
+  );
+}
+
+void f1() {
+  print('ElevatedButton btn click');
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+      AppBar(
+        title: Text("test",),
+        centerTitle: true,
+        // backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.deepPurple,
+      ),
+      // body: Text('test message'),
+      body:
+        // Image.asset(
+        //   '../assets/1.jpg', height: 120, width: 120,
+        //   // fit: BoxFit.fill,
+        //   fit: BoxFit.cover,
+        //   // repeat: ImageRepeat.repeat,
+        // ),
+
+        // Image.network('https://ts1.cn.mm.bing.net/th/id/R-C.1f5914fce91db58cd69dd3079cf577fd?rik=h0ejjMK7AuWeZg&riu=http%3a%2f%2fwww.brooklyncomicshop.com%2fBCS%2fwp-content%2fuploads%2f2019%2f02%2fAction-Comics-419-Brooklyn-Comic-Shop.jpg.jpeg&ehk=sL0vX4cvv1WKTcd1MO6toEy4Oi8PpKrvK5G4Hy4xEdc%3d&risl=&pid=ImgRaw&r=0',
+        // width: 120, height: 120, fit: BoxFit.fill,)
+
+        // CircleAvatar(backgroundImage: AssetImage('../assets/2.webp'), radius: 50,)
+
+        // FadeInImage(placeholder: AssetImage('../assets/3.webp'), image: NetworkImage('https://ts1.cn.mm.bing.net/th/id/R-C.1f5914fce91db58cd69dd3079cf577fd?rik=h0ejjMK7AuWeZg&riu=http%3a%2f%2fwww.brooklyncomicshop.com%2fBCS%2fwp-content%2fuploads%2f2019%2f02%2fAction-Comics-419-Brooklyn-Comic-Shop.jpg.jpeg&ehk=sL0vX4cvv1WKTcd1MO6toEy4Oi8PpKrvK5G4Hy4xEdc%3d&risl=&pid=ImgRaw&r=0'),)
+        Row(
+          children: [
+            Text('text1'),
+            Text('text2'),
+            Text('text3'),
+            Text('text4'),
+            Text('text5'),
+            Column(
+              children: [
+                Text('text6'),
+                Text('text7'),
+                Image.asset(
+                  '../assets/1.jpg', height: 120, width: 120,
+                  // fit: BoxFit.fill,
+                  fit: BoxFit.cover,
+                  // repeat: ImageRepeat.repeat,
+                ),
+                ElevatedButton(
+                  onPressed: (){f1();},
+                  child: Text('ElevatedButton'),
+                  // 两种设置样式的方式
+                  // style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.amber)),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.cyanAccent),
+                ),
+                TextButton(
+                  onPressed: (){print('TextButton btn click');},
+                  child: Text('TextButton')),
+                OutlinedButton(
+                  onPressed: (){print('OutlinedButton btn click');},
+                  child: Text('OutlinedButton')),
+              ],
+            ),
+            Text('text8'),
+            Text('text9'),
+          ],
+        ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){print('FloatingActionButton btn click');},
+          child: Icon(Icons.add),
+      )
+    );
+  }
+
+}
+
